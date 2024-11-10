2c0b6fc19c9da86b842b0f72d03ccb9e119b46b4
CoderTofu <vioneverforget@gmail.com>
2024-11-10 17:59:21 +0800

    completed banner

--- resources/js/Elements/Navbar.jsx
+++ resources/js/Elements/Navbar.jsx
@@ -27,20 +27,20 @@ export default function Navbar() {
             <nav className="-mx-3 flex items-center justify-center">
                 <Link
                     href={route("login")}
-                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] "
+                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:opacity-75 focus:outline-none focus-visible:ring-[#FF2D20] "
                 >
                     <img
-                        className="max-w-[80px] h-auto justify-self-center"
+                        className="h-11 border-gray-800 justify-self-center border p-2 rounded-full"
                         src="/assets/account-icon.png"
                         alt="Account Icon"
                     />
                 </Link>
                 <Link
                     href={route("products")}
-                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] "
+                    className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:opacity-75 focus:outline-none focus-visible:ring-[#FF2D20] "
                 >
                     <img
-                        className="max-w-[80px] h-auto justify-self-center"
+                        className="h-9 justify-self-center"
                         src="/assets/cart-icon.png"
                         alt="Cart Icon"
                     />
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,11 +1,63 @@
 import { Head } from "@inertiajs/react";
 import Navbar from "../Elements/Navbar";
 
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "@/components/ui/carousel";
+
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    const carouselItems = [
+        "https://www.yellowbrick.co/wp-content/uploads/2023/02/Streetwear-style.jpg",
+        "https://media.voguebusiness.com/photos/661033e2da7d9e9fd8ddfea1/master/w_1600%2Cc_limit/STREETWEAR-IN-2024-vogue-business-story-inline-6.jpg",
+        "https://www.highsnobiety.com/static-assets/dato/1690970245-inside-nigerias-booming-streetwear-renaissance-07.jpg",
+    ];
+
     return (
         <>
             <Head title="Home" />
             <Navbar />
+            <Carousel
+                opts={{ loop: true }}
+                plugins={[
+                    Autoplay({
+                        delay: 7000,
+                    }),
+                ]}
+                className="w-full flex justify-between items-center bg-gray-700"
+            >
+                <CarouselPrevious className="absolute left-5 z-20 border-none bg-white rounded cursor-pointer transition-colors hover:bg-gray-200" />
+
+                <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center z-10">
+                    <p className="text-white text-5xl font-bold">
+                        Discover the Latest in Streetwear Fashion
+                    </p>
+                </div>
+
+                <CarouselContent className="z-0">
+                    {carouselItems.map((item, index) => (
+                        <CarouselItem key={index}>
+                            <div className="w-full h-full max-h-[80vh]">
+                                <Card className="w-full h-full max-h-[100%] border-none">
+                                    <CardContent className="flex items-center justify-center w-full h-full max-h-[100%]">
+                                        <img
+                                            src={item}
+                                            alt={`Carousel item ${index + 1}`}
+                                            className="object-cover w-full h-full max-w-[100%] object-top"
+                                        />
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselNext className="absolute right-5 z-20 border-none bg-white rounded cursor-pointer transition-colors hover:bg-gray-200" />
+            </Carousel>
         </>
     );
 }
