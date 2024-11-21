40be0ba261c3e0d72d586ea04428c13df7720306
CoderTofu <vioneverforget@gmail.com>
2024-11-21 15:25:51 +0800

    minor changes

--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -1,12 +1,16 @@
 import { Link } from "@inertiajs/react";
 import Navbar from "../Elements/Navbar";
+import Footer from "./../Elements/Footer";
 
 export default function GuestLayout({ children }) {
     return (
         <>
             <Navbar />
-            <div className="flex h-5/6 flex-col items-center  pt-6 sm:justify-center sm:pt-0">
+            <div className="flex h-5/6 flex-col items-center pt-6 sm:justify-center sm:pt-0">
                 {children}
+            </div>
+            <div className="pt-[20vh]">
+                <Footer />
             </div>
         </>
     );
