4360e53c8c3ea98493726b15694a82f3f631b9a1
Rafael Torres <rafaelsebastian0908@gmail.com>
2024-11-28 20:09:53 +0800

    add backend for phone and address update

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -25,6 +25,12 @@ class ProfileUpdateRequest extends FormRequest
                 'max:255',
                 Rule::unique(User::class)->ignore($this->user()->id),
             ],
+            'phone' => [
+                'string', 'max:11'
+            ],
+            'address' => [
+                'string', 'max:255'
+            ]
         ];
     }
 }
